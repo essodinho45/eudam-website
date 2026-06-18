6ca19ed5bddf7ed8c2df73a2b41bad6412c8bda0
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2026-06-18 10:37:31 +0300

    ww

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\App;
 
 // Language Switcher Route
 Route::get('lang/{locale}', function ($locale) {
